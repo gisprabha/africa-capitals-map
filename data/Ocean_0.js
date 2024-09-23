b236ce63951436feb9ab5edfadc33ee4ccffcf32
gisprabha <gisprabhashini@gmail.com>
2024-09-23 23:46:36 +0530

    updates

---
+++ data/Ocean_0.js
@@ -0,0 +1,8 @@
+var json_Ocean_0 = {
+"type": "FeatureCollection",
+"name": "Ocean_0",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "FID": "0" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -69.258211585872985, 85.009537022655223 ], [ 99.772670200463665, 85.762458545088805 ], [ 99.019748678030112, -80.256737151513576 ], [ -68.881750824656194, -80.633197912730381 ], [ -69.258211585872985, 85.009537022655223 ] ] ] ] } }
+]
+}
